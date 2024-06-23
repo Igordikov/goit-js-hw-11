214f9e24301a247b35b6edcf111f70482022afcc
Igor <igorivandikov87@gmail.com>
2024-06-23 16:21:26 +0300

    form style

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// izitoast
+import iziToast from  "izitoast" ;
+
+import  "izitoast/dist/css/iziToast.min.css" ;
+
+// simplelightbox
+
+
+import SimpleLightbox from  "simplelightbox" ;
+ 
+import  "simplelightbox/dist/simple-lightbox.min.css" ;
+
+
+// ============================================================
+
